9e289c0e110b7604f3e3b98b2fe4ee5a2e714324
pelumiay <10568500@mydbs.ie>
2021-08-03 18:23:06 +0100

    Updated init

--- market/__init__.py
+++ market/__init__.py
@@ -4,17 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__) #creates a flask app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' #configure myshop database
+
+
+#app = Flask(__name__)
 app.config['SECRET_KEY'] = '6bdbe190331388b4adf0d2a2'
 
-ENV = 'prod'
+#ENV = 'prod'
 
 
-if ENV == 'dev':
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+#if ENV == 'dev':
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fekjtgfydxieoe:b388d656a0f5214a3dbff4cacc5083d437c8047320c535f72a4d9eac7adc62ca@ec2-52-6-77-239.compute-1.amazonaws.com:5432/d1kkbe89td43jm'
+#else:
+   # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fekjtgfydxieoe:b388d656a0f5214a3dbff4cacc5083d437c8047320c535f72a4d9eac7adc62ca@ec2-52-6-77-239.compute-1.amazonaws.com:5432/d1kkbe89td43jm'
     
     
 db = SQLAlchemy(app)
