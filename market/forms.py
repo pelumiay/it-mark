5331b716ef61a7e4cf01d62851c78e58ce0fd9d8
pelumiay <79277228+pelumiay@users.noreply.github.com>
2021-08-06 05:19:05 +0100

    Update forms.py

--- market/forms.py
+++ market/forms.py
@@ -37,7 +37,7 @@ class addproductForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
-    password = Password(label='Password:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign in')
 
 class PurchaseItemForm(FlaskForm):
